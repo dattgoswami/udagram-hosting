d44f8a689a03caa6682ae5e1f4e5f5ba41e0edcc
Datt Goswami <dattgoswami@gmail.com>
2021-09-03 07:53:45 -0600

    updated yml for circle ci

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   appName: "Udagram",
   // apiHost: "http://localhost:8080/api/v0",
   apiHost:
-    "http://Udagramapiserver-env.eba-enimaink.us-east-1.elasticbeanstalk.com/api/v0",
+    "http://Masterudagram-env.eba-jvptymbj.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
