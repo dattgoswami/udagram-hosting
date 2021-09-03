6a95b953b490fee1d7c5412315735ea949db19b4
Datt Goswami <dattgoswami@gmail.com>
2021-09-03 05:29:22 -0600

    updated circle ci's config

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,9 +5,7 @@
 export const environment = {
   production: false,
   appName: "Udagram",
-  // apiHost: "http://localhost:8080/api/v0",
-  apiHost:
-    "http://Ebsudagramenv-env.eba-kixy4xhm.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://localhost:8080/api/v0",
 };
 
 /*
